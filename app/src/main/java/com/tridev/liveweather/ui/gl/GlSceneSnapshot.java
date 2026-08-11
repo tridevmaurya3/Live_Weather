79bccc42e4a499d38b0b8a89ccd0c0461c6f23d2
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-08-11 16:11:25 +0530

    Add OpenGL scene snapshot preserving astronomy and weather reality

---
+++ app/src/main/java/com/tridev/liveweather/ui/gl/GlSceneSnapshot.java
@@ -0,0 +1,112 @@
+package com.tridev.liveweather.ui.gl;
+
+/**
+ * Immutable GPU-facing snapshot. All values are normalized before they cross
+ * into the OpenGL renderer so the render loop never needs network/location work.
+ */
+public final class GlSceneSnapshot {
+
+    public final float topR;
+    public final float topG;
+    public final float topB;
+    public final float midR;
+    public final float midG;
+    public final float midB;
+    public final float horizonR;
+    public final float horizonG;
+    public final float horizonB;
+
+    public final float sunX;
+    public final float sunY;
+    public final float sunVisibility;
+    public final float sunAltitude;
+
+    public final float moonX;
+    public final float moonY;
+    public final float moonVisibility;
+    public final float moonIllumination;
+    public final float moonPhaseAngleRadians;
+    public final float moonAltitude;
+
+    public final float starVisibility;
+    public final float cloudCover;
+    public final float rainIntensity;
+    public final float drizzleIntensity;
+    public final float snowIntensity;
+    public final float fogIntensity;
+    public final float stormIntensity;
+    public final float airHazeIntensity;
+    public final float windStrength;
+    public final float windDirectionRadians;
+    public final float sceneLight;
+    public final float visibilityFactor;
+    public final float parallax;
+
+    public GlSceneSnapshot(
+            float topR,
+            float topG,
+            float topB,
+            float midR,
+            float midG,
+            float midB,
+            float horizonR,
+            float horizonG,
+            float horizonB,
+            float sunX,
+            float sunY,
+            float sunVisibility,
+            float sunAltitude,
+            float moonX,
+            float moonY,
+            float moonVisibility,
+            float moonIllumination,
+            float moonPhaseAngleRadians,
+            float moonAltitude,
+            float starVisibility,
+            float cloudCover,
+            float rainIntensity,
+            float drizzleIntensity,
+            float snowIntensity,
+            float fogIntensity,
+            float stormIntensity,
+            float airHazeIntensity,
+            float windStrength,
+            float windDirectionRadians,
+            float sceneLight,
+            float visibilityFactor,
+            float parallax
+    ) {
+        this.topR = topR;
+        this.topG = topG;
+        this.topB = topB;
+        this.midR = midR;
+        this.midG = midG;
+        this.midB = midB;
+        this.horizonR = horizonR;
+        this.horizonG = horizonG;
+        this.horizonB = horizonB;
+        this.sunX = sunX;
+        this.sunY = sunY;
+        this.sunVisibility = sunVisibility;
+        this.sunAltitude = sunAltitude;
+        this.moonX = moonX;
+        this.moonY = moonY;
+        this.moonVisibility = moonVisibility;
+        this.moonIllumination = moonIllumination;
+        this.moonPhaseAngleRadians = moonPhaseAngleRadians;
+        this.moonAltitude = moonAltitude;
+        this.starVisibility = starVisibility;
+        this.cloudCover = cloudCover;
+        this.rainIntensity = rainIntensity;
+        this.drizzleIntensity = drizzleIntensity;
+        this.snowIntensity = snowIntensity;
+        this.fogIntensity = fogIntensity;
+        this.stormIntensity = stormIntensity;
+        this.airHazeIntensity = airHazeIntensity;
+        this.windStrength = windStrength;
+        this.windDirectionRadians = windDirectionRadians;
+        this.sceneLight = sceneLight;
+        this.visibilityFactor = visibilityFactor;
+        this.parallax = parallax;
+    }
+}
