24eecbe567dea0eb0f00e82c8e533c6dbafa9f15
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-08-11 07:21:31 +0530

    Add air quality Retrofit client

---
+++ app/src/main/java/com/tridev/liveweather/data/remote/api/AirQualityApiClient.java
@@ -0,0 +1,32 @@
+package com.tridev.liveweather.data.remote.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public final class AirQualityApiClient {
+
+    private static final String BASE_URL = "https://air-quality-api.open-meteo.com/";
+    private static volatile AirQualityApiService apiService;
+
+    private AirQualityApiClient() {
+    }
+
+    public static AirQualityApiService getApiService() {
+        if (apiService == null) {
+            synchronized (AirQualityApiClient.class) {
+                if (apiService == null) {
+                    Gson gson = new GsonBuilder().create();
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl(BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create(gson))
+                            .build();
+                    apiService = retrofit.create(AirQualityApiService.class);
+                }
+            }
+        }
+        return apiService;
+    }
+}
